b65ef69106f01106a6996c50c2387a797c37457e
SidahmedYS <sidahmedys10@gmail.com>
2019-01-30 12:02:43 +0100

    Le modele de données complété

--- Etudiant_et_Doctorant/etudiants_et_doctorants.py
+++ Etudiant_et_Doctorant/etudiants_et_doctorants.py
@@ -6,17 +6,18 @@ class etudiant(models.Model):
     dateN=fields.date('dateN',size=30,required=True, help='the birth date')
     prenom=fields.char('prenom',size=50, help='the email')
     id_encadreur=many2one('encadreur')
+    sessionEncadrementEtudiant_ids=one2many('sessionEncadrementEtudiant','id_etudiant')
 
 etudiant()
 class doctorant(models.Model):
     _name = 'doctorant'
-    doctorant_id=fields.integer('doctorant_id',size=30,required=True, help='the name')
     nom=fields.char('first name',size=30,required=True, help='the first name')
     dateN=fields.date('date',size=30,required=True, help='the birth date')
     prenom=fields.char('email',size=50, help='the email')
     etat=fields.char('etat',size=50,help='the stat of the doctorant')
     id_encadreurD = many2one('encadreur')
     id_these=many2one('these')
+    sessionEncadrementDoctorant_ids = one2many('sessionEncadrementDoctorant', 'id_doctorant')
 
 doctorant()
 class encadreur(models.Model):
@@ -28,15 +29,52 @@ class encadreur(models.Model):
     etudiant_ids = one2many('etudiant','id_encadreur')
     doctorant_ids = one2many('doctorant', 'id_encadreurD')
 
+encadreur()
+class jury(models.Model):
+    _name = 'jury'
+    nom=fields.char('nom',size=30,required=True, help='the name')
+    prenom=fields.char('prenom',size=30,required=True, help='the first name')
+    dateN=fields.date('dateN',size=30,required=True, help='the birth date')
+    fonction=fields.char('fonction',size=50, help='the job')
+    sessionEvaluation_ids = one2many('sessionEvaluation','jury_ids')
+
+jury()
 class these(models.Model):
     _name = 'these'
     titre=fields.char('titre',size=30,required=True, help='the title')
     description=fields.char('description',size=30,required=True, help='the description')
     doctorant_ids = one2many('doctorant','id_these')
 
+class formulaireE(models.Model):
+    _name = 'formulaireE'
+    note=fields.integer('note',size=30,required=True, help='the mark')
+    motifs=fields.char('motifs',size=30,required=True, help='motives')
+    remarques=fields.char('remarques',size=50, help='the notes')
+    sessionEvaluation_id = many2one('sessionEvaluation')
+    jury_id = many2one('jury')
+
 class sessionEvaluation(models.Model):
     _name = 'sessionEvaluation'
     heure=fields.char('titre',size=30,required=True, help='the title')
     date = fields.date('date', size=30, required=True)
     lieux = fields.char('lieux',size=30,required=True)
+    jury_ids=many2one('jury')
+    formulaireE_ids = one2many('formulaireE', 'sessionEvaluation_id')
+
+
+class sessionEncadrementEtudiant(models.Model):
+    _name = 'sessionEncadrement'
+    heure=fields.char('titre',size=30,required=True, help='the time')
+    date = fields.date('date', size=30, required=True)
+    etatAvancement = fields.char('etatAvancement', size=50, help='')
+    remarques = fields.char('remarques', size=50, help='the notes')
+    id_etudiant=many2one('etudiant')
+
+class sessionEncadrementDoctorant(models.Model):
+    _name = 'sessionEncadrement'
+    heure=fields.char('titre',size=30,required=True, help='the time')
+    date = fields.date('date', size=30, required=True)
+    etatAvancement = fields.char('etatAvancement', size=50, help='')
+    remarques = fields.char('remarques', size=50, help='the notes')
+    id_doctorant=many2one('doctorant')
 
