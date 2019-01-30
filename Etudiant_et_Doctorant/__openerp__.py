0a8bd475d854ef025f695dbbc5e3e19f830dabdf
SidahmedYS <sidahmedys10@gmail.com>
2019-01-31 00:11:11 +0100

    Finalisation et correction de quelques erreurs pour le module Etudiants et Doctorants

--- Etudiant_et_Doctorant/__openerp__.py
+++ Etudiant_et_Doctorant/__openerp__.py
@@ -1,15 +1,12 @@
-{
-    "name" : "Etudiants et Doctorants",
-    "version" : "1.0",
-    "author" : "YallesSidahmed",
-    "website" : "http://www.esi.dz/",
-    "sequence" : 1,
-    "category" : "DPGR",
-    "summary" : "Module qui gére les étudiants et les doctorants",
-    "description": """ Module qui gére les étudiants et les doctorants """,
-    "depends" : ['base'],
-    "init_xml" : [ ],
-    "demo_xml" : [ ],
-    'data' : ['etudiants_et_doctorants.xml'],
-    "installable": True
+﻿{
+    'name': 'Etudiant',
+    'version': '0.1',
+    'category': 'Generic Modules/Others',
+    'description': """ Module pour la DPGR
+    """,
+    'author': 'YALLES',
+    'depends': ['base'],
+    'data': ['etudiants_et_doctorants.xml'],
+    'installable': True,
+    'auto_install': False,
 }
--- Etudiant_et_Doctorant/etudiants_et_doctorants.py
+++ Etudiant_et_Doctorant/etudiants_et_doctorants.py
@@ -1,86 +1,78 @@
 from openerp import fields,models
 class etudiant(models.Model):
-    _name = 'etudiant'
-    matricule=fields.char('matricule',size=30,required=True, help='the name')
-    nom=fields.char('nom',size=30,required=True, help='the first name')
-    #n'utilise pas les majuscules
-    dateN=fields.date('dateN',size=30,required=True, help='the birth date')
-    prenom=fields.char('prenom',size=50, help='the email')
-    id_encadreur=many2one('encadreur')
-    #n'utilise pas les majuscules
-    sessionEncadrementEtudiant_ids=one2many('sessionEncadrementEtudiant','id_etudiant')
+    _name = 'dpgr.etudiant'
+    matricule=fields.Integer('matricule',size=30,required=True, help='the name')
+    nom=fields.Char('nom',size=30,required=True, help='the first name')
+    dateN=fields.Date('dateN',size=30,required=True, help='the birth date')
+    prenom=fields.Char('prenom',size=50, help='the email')
+    id_encadreur=fields.Many2one('dpgr.encadreur')
+    sessionencadrementetu_ids=fields.One2many('dpgr.sessionencadrementetu','id_etudiant')
 
-etudiant()
 class doctorant(models.Model):
-    _name = 'doctorant'
-    nom=fields.char('first name',size=30,required=True, help='the first name')
-    dateN=fields.date('date',size=30,required=True, help='the birth date')
-    prenom=fields.char('email',size=50, help='the email')
-    etat=fields.char('etat',size=50,help='the stat of the doctorant')
-    id_encadreurD = many2one('encadreur')
-    id_these=many2one('these')
-    sessionEncadrementDoctorant_ids = one2many('sessionEncadrementDoctorant', 'id_doctorant')
-    sessionEvaluation_ids = one2many('sessionEvaluation', 'doctorant_id')
+    _name = 'dpgr.doctorant'
+    nom=fields.Char('nom',size=30,required=True, help='the first name')
+    dateN=fields.Date('date',size=30,required=True, help='the birth date')
+    prenom=fields.Char('email',size=50, help='the email')
+    etat=fields.Char('etat',size=50,help='the stat of the doctorant')
+    id_encadreurD = fields.Many2one('dpgr.encadreur')
+    id_these=fields.Many2one('dpgr.these')
+    sessionencadrementdoc_ids = fields.One2many('dpgr.sessionencadrementdoc', 'id_doc')
+    sessionevaluation_ids = fields.One2many('dpgr.sessionevaluation', 'doctorant_id')
 
-doctorant()
 class encadreur(models.Model):
-    _name = 'encadreur'
-    nom=fields.char('nom',size=30,required=True, help='the name')
-    prenom=fields.char('prenom',size=30,required=True, help='the first name')
-    dateN=fields.date('dateN',size=30,required=True, help='the birth date')
-    fonction=fields.char('fonction',size=50, help='the job')
-    etudiant_ids = one2many('etudiant','id_encadreur')
-    doctorant_ids = one2many('doctorant', 'id_encadreurD')
+    _name = 'dpgr.encadreur'
+    nom=fields.Char('nom',size=30,required=True, help='the name')
+    prenom=fields.Char('prenom',size=30,required=True, help='the first name')
+    dateN=fields.Date('dateN',size=30,required=True, help='the birth date')
+    fonction=fields.Char('fonction',size=50, help='the job')
+    etudiant_ids = fields.One2many('dpgr.etudiant','id_encadreur')
+    doctorant_ids = fields.One2many('dpgr.doctorant', 'id_encadreurD')
 
-encadreur()
 class jury(models.Model):
-    _name = 'jury'
-    nom=fields.char('nom',size=30,required=True, help='the name')
-    prenom=fields.char('prenom',size=30,required=True, help='the first name')
-    dateN=fields.date('dateN',size=30,required=True, help='the birth date')
-    fonction=fields.char('fonction',size=50, help='the job')
-    sessionEvaluation_ids = one2many('sessionEvaluation','jury_id')
+    _name = 'dpgr.jury'
+    nom=fields.Char('nom',size=30,required=True, help='the name')
+    prenom=fields.Char('prenom',size=30,required=True, help='the first name')
+    dateN=fields.Date('dateN',size=30,required=True, help='the birth date')
+    fonction=fields.Char('fonction',size=50, help='the job')
+    sessionevaluation_ids = fields.One2many('dpgr.sessionevaluation','jury_id')
 
-jury()
 class these(models.Model):
-    _name = 'these'
-    titre=fields.char('titre',size=30,required=True, help='the title')
-    description=fields.char('description',size=30,required=True, help='the description')
-    doctorant_ids = one2many('doctorant','id_these')
+    _name = 'dpgr.these'
+    titre=fields.Char('titre',size=30,required=True, help='the title')
+    description=fields.Char('description',size=30,required=True, help='the description')
+    doctorant_ids = fields.One2many('dpgr.doctorant','id_these')
 
-these()
-class formulaireE(models.Model):
-    _name = 'formulaireE'
-    note=fields.integer('note',size=30,required=True, help='the mark')
-    motifs=fields.char('motifs',size=30,required=True, help='motives')
-    remarques=fields.char('remarques',size=50, help='the notes')
-    sessionEvaluation_id = one2many('sessionEvaluation','formulaireE_id')
+class formulaire(models.Model):
+    _name = 'dpgr.formulaire'
+    note=fields.Integer('note',size=30,required=True, help='the mark')
+    motifs=fields.Char('motifs',size=30,required=True, help='motives')
+    remarques=fields.Char('remarques',size=50, help='the notes')
+    sessionevaluation_id = fields.One2many('dpgr.sessionevaluation','formulaire_id')
 
-formulaireE()
-class sessionEvaluation(models.Model):
-    _name = 'sessionEvaluation'
-    heure=fields.char('titre',size=30,required=True, help='the title')
-    date = fields.date('date', size=30, required=True)
-    lieux = fields.char('lieux',size=30,required=True)
-    doctorant_id=fields.many2one('doctorant')
-    jury_id=many2one('jury')
-    formulaireE_id = many2one('formulaireE')
+class sessionevaluation(models.Model):
+    _name = 'dpgr.sessionevaluation'
+    heure=fields.Float('titre',size=30,required=True)
+    date = fields.Date('date', size=30, required=True)
+    lieux = fields.Char('lieux',size=30,required=True)
+    doctorant_id=fields.Many2one('dpgr.doctorant')
+    jury_id=fields.Many2one('dpgr.jury')
+    formulaire_id = fields.Many2one('dpgr.formulaire')
 
 
-class sessionEncadrementEtudiant(models.Model):
-    _name = 'sessionEncadrement'
-    heure=fields.char('titre',size=30,required=True, help='the time')
-    date = fields.date('date', size=30, required=True)
-    etatAvancement = fields.char('etatAvancement', size=50, help='')
-    remarques = fields.char('remarques', size=50, help='the notes')
-    id_etudiant=many2one('etudiant')
+class sessionencadrementetu(models.Model):
+    _name = 'dpgr.sessionencadrementetu'
+    heure=fields.Char('titre',size=30,required=True, help='the time')
+    date = fields.Date('date', size=30, required=True)
+    etatAvancement = fields.Char('etatAvancement', size=50, help='')
+    remarques = fields.Char('remarques', size=50, help='the notes')
+    id_etudiant=fields.Many2one('dpgr.etudiant')
 
-class sessionEncadrementDoctorant(models.Model):
-    # Pour les noms des classes utiliser les _
-    _name = 'sessionEncadrement'
-    heure=fields.char('titre',size=30,required=True, help='the time')
-    date = fields.date('date', size=30, required=True)
-    etatAvancement = fields.char('etatAvancement', size=50, help='')
-    remarques = fields.char('remarques', size=50, help='the notes')
-    id_doctorant=many2one('doctorant')
+class sessionencadrementdoc(models.Model):
+    _name = 'dpgr.sessionencadrementdoc'
+    heure=fields.Char('titre',size=30,required=True, help='the time')
+    date = fields.Date('date', size=30, required=True)
+    etatAvancement = fields.Char('etatAvancement', size=50, help='')
+    remarques = fields.Char('remarques', size=50, help='the notes')
+    id_doc=fields.Many2one('dpgr.doctorant')
+
 
